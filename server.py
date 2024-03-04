71e5e50998dfc9c76fafcc5bec6f28a29d09994e
Igor <Aleshichev.git@email.ua>
2024-03-04 12:30:44 +0200

    ivent loop single thread

--- server.py
+++ server.py
@@ -1,26 +1,36 @@
 import socket
 import threading
+import select
 
 
 def handle(s):
-    while True:
-        data = s.recv(1024)  # размер буфера
-        if not data:
-            s.close()
-            break
-        print(data)
-        s.sendall(data)
+
+    data = s.recv(1024)  # размер буфера
+    if not data:
+        connections.remove(s)
+        s.close()
+        return
+    print(data)
+    s.sendall(data)
 
 
 s = socket.socket()
+s.setblocking(False)  # неблокирующий сокет
 s.bind(("127.0.0.1", 5000))
 s.listen(5)
+connections = [s]
 print("Watting for connections")
 try:
     while True:
-        c, a = s.accept()  # ждёт подключение с-клиентский сокет а-адресс
-        print("Connected", a)
-        t = threading.Thread(target=handle, args=(c,))  # создаём поток
-        t.start()
+        r_s, _, _ = select.select(
+            connections, [], []
+        )  # список сокетов готовых для: чтения / записи/ с ошибками
+        for r in r_s:
+            if r == s:
+                c, a = s.accept()  # ждёт подключение с-клиентский сокет а-адресс
+                print("Connected", a)
+                connections.append(c)
+            else:
+                handle(r)
 finally:
     s.close()
