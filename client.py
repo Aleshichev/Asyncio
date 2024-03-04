6cd80c1ec9317bafd1b6f39c296d0c67ff18add5
Igor <Aleshichev.git@email.ua>
2024-03-04 11:44:07 +0200

    classic threading server

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
 import socket
+import time
 
 s = socket.socket()
-s.connect(('127.0.0.1', 5000))
-s.sendall(b"Hello")
-data = s.recv(1024)
-print(data)
-s.close()
+s.connect(("127.0.0.1", 5000))
+try:
+    while True:
+        s.sendall(b"Hello")
+        data = s.recv(1024)
+        print(data)
+        time.sleep(2)
+finally:
+    s.close()
--- server.py
+++ server.py
@@ -1,13 +1,26 @@
 import socket
+import threading
+
+
+def handle(s):
+    while True:
+        data = s.recv(1024)  # размер буфера
+        if not data:
+            s.close()
+            break
+        print(data)
+        s.sendall(data)
+
 
 s = socket.socket()
 s.bind(("127.0.0.1", 5000))
 s.listen(5)
 print("Watting for connections")
-c, a = s.accept()      #ждёт подключение с-клиентский сокет а-адресс 
-print("Connected", a)
-data = c.recv(1024)    #размер буфера
-print(data)
-c.sendall(data)
-c.close()
-s.close()
+try:
+    while True:
+        c, a = s.accept()  # ждёт подключение с-клиентский сокет а-адресс
+        print("Connected", a)
+        t = threading.Thread(target=handle, args=(c,))  # создаём поток
+        t.start()
+finally:
+    s.close()
